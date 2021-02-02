c32e661880411406229c24769386aa8d47fca589
Álvaro Maestre <alvaro.maestre-santa@capgemini.com>
2021-02-02 21:12:31 +0100

    Creación del módulo padre

---
+++ multimodulo-spring-jpa/src/main/java/es/capgemini/curso/multimodulo/spring/jpa/App.java
@@ -0,0 +1,13 @@
+package es.capgemini.curso.multimodulo.spring.jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
