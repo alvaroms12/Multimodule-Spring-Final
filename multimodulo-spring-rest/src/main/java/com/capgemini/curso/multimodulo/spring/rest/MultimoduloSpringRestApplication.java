e83013a43ba15d32614cfce4d37c8aa69e4dd682
Álvaro Maestre <alvaro.maestre-santa@capgemini.com>
2021-02-04 17:54:11 +0100

    Creación del proyecto rest del ejercicio propuesto

---
+++ multimodulo-spring-rest/src/main/java/com/capgemini/curso/multimodulo/spring/rest/MultimoduloSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.curso.multimodulo.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimoduloSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimoduloSpringRestApplication.class, args);
+	}
+
+}
