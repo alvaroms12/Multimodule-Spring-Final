8275acafa07c72ef9603bf3c85143fa23c584a5c
Álvaro Maestre <alvaro.maestre-santa@capgemini.com>
2021-02-06 20:28:24 +0100

    Creación del controlador spring mvc

--- multimodulo-spring-mvc/src/main/java/es/capgemini/curso/multimodulo/spring/mvc/web/ControllerWeb.java
+++ multimodulo-spring-mvc/src/main/java/es/capgemini/curso/multimodulo/spring/mvc/web/ControllerWeb.java
@@ -2,9 +2,14 @@ package es.capgemini.curso.multimodulo.spring.mvc.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import es.capgemini.curso.multimodulo.spring.jpa.modelo.entidades.Post;
 import es.capgemini.curso.spring.servicios.PostService;
 
 @Controller
@@ -16,5 +21,33 @@ public class ControllerWeb {
 	@GetMapping("/")
 	public ModelAndView index() {
 		return new ModelAndView("index", "post", services.getAll());
+	}
+	
+	@GetMapping("/addPost")
+	public ModelAndView newPost() {
+		return new ModelAndView("newPost", "post", new Post());
+	}
+	
+	@PostMapping("/addPost")
+	public String saveNewPost(@ModelAttribute("post") Post post) {
+		services.add(post);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/updatePost")
+	public ModelAndView updatePost(@RequestParam("id") int id) {
+		return new ModelAndView("updatePost", "post", services.getById(id).get());
+	}
+	
+	@PostMapping("/updatePost")
+	public String saveUpdatePost (@ModelAttribute("post") Post post) {
+		services.edit(post);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/deletePost")
+	public String deletePost (@RequestParam("id") int id) {
+		services.remove(services.getById(id).get());
+		return "redirect:/";
 	}
 }
